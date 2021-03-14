3d6a31684404a561cbcb544bba317af04e1f89d2
Ivan Grinenko <standfer231@gmail.com>
2021-03-14 13:30:22 +0400

    1178 commit2 1178 commit3 1178 commit4 1178 commit5

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("test");
-        //1178 change1
+        //1178 change2
+        //1178 change4
+        //1178 change5
     }
 
     private void testMethod() {
@@ -21,19 +23,7 @@ public class Main {
         String test3 = "test3";
     }
 
-    private void branch1() {
-        String test1 = "test1";
-        String test2 = "test2";
-        String test3 = "test3";
-    }
-
-    private void branch2() {
-        String test1 = "test1";
-        String test2 = "test2";
-    }
-
-    private void branch3() {
-        String test1 = "test1";
-        String test2 = "test2";
+    private void boxRelease424() {
+        //boxRelease424 commit1
     }
 }
